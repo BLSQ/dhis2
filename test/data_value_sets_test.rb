0bc0c4c2c0c855baae2b7c27140695c3b2c0f145
Martin Van Aken <martin@joyouscoding.com>
2016-09-12 22:52:35 +0200

    small indentation fixes

--- test/data_value_sets_test.rb
+++ test/data_value_sets_test.rb
@@ -6,7 +6,11 @@ class DataValueSetsTest < Minitest::Test
     organisation_unit = Dhis2::OrganisationUnit.find_by(name: "Baoma")
     period = "201512"
 
-    value_sets = Dhis2::DataValueSets.list(data_sets: [ds.id], organisation_unit: organisation_unit.id, periods: [period])
+    value_sets = Dhis2::DataValueSets.list(
+      data_sets:         [ds.id],
+      organisation_unit: organisation_unit.id,
+      periods:           [period]
+    )
     assert_equal ds.id, value_sets.dataSet
     assert_equal period, value_sets.period
     assert_equal organisation_unit.id, value_sets.orgUnit
@@ -25,7 +29,11 @@ class DataValueSetsTest < Minitest::Test
     org_units = Dhis2::OrganisationUnit.list(page_size: 2)
     data_elements = Dhis2::DataElement.list(page_size: 2)
 
-    tuple = [{dataElement: data_elements.first.id, orgUnit: org_units.first.id, period: "201601", value: 36}]
+    tuple = [{
+      dataElement: data_elements.first.id,
+      orgUnit: org_units.first.id,
+      period: "201601", value: 36
+    }]
     status = Dhis2::DataValueSets.create(tuple)
     assert_equal true, status.success?
   end
