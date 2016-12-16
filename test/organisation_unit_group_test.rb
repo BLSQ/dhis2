74812682be9f9708d1d816f3fa5b83df35e65ff0
HAKIZIMANA Franck <fhakizimana@bluesquare.org>
2016-12-16 13:50:12 +0200

    add to dhis2 client the feature of creating orgunitgroups

--- test/organisation_unit_group_test.rb
+++ test/organisation_unit_group_test.rb
@@ -18,4 +18,15 @@ class OrganisationUnitGroupTest < Minitest::Test
     organisation_unit_group = Dhis2.client.organisation_unit_groups.find_by(name: "CHC")
     assert_equal "CHC", organisation_unit_group.display_name
   end
+
+  def test_create_org_unit_groups
+    org_unit_group_name_1 = SecureRandom.uuid
+    org_unit_groups = {name: org_unit_group_name_1, short_name: org_unit_group_name_1, code:"test", id:"Ysjldlkdsflksfs"}
+    status = Dhis2.client.organisation_unit_groups.create(org_unit_groups)
+    assert_equal false, status.success?
+    assert_equal 1, status.total_imported
+    org_unit_group_1 = Dhis2.client.organisation_unit_groups.list(fields: :all, filter: "name:eq:#{org_unit_group_name_1}").first
+    assert_equal org_unit_group_name_1, org_unit_group_1.short_name
+    assert_equal "Ysjldlkdsflksfs", org_unit_group_1.uid
+  end
 end
