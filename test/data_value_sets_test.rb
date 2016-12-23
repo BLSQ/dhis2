3f525dad17ef159ab9e7dce9e6de59c1050e751e
Stéphan Mestach <smestach@bluesquare.org>
2016-12-23 15:09:08 +0100

    Support more generic api for data values retrieval by where/what/when.

--- test/data_value_sets_test.rb
+++ test/data_value_sets_test.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "test_helper"
 
 class DataValueSetsTest < Minitest::Test
@@ -41,6 +42,59 @@ class DataValueSetsTest < Minitest::Test
     refute_nil value.data_element
     refute_nil value.period
     refute_nil value.org_unit
+  end
+
+  def test_should_build_list_url_for_oug_deg_start_end
+    url = Dhis2.client.data_value_sets.build_list_url(
+      organisation_unit_group: "oRVt7g429ZO",
+      data_element_groups:     ["oDkJh5Ddh7d"],
+      start_date:              Date.parse("31-1-2015"),
+      end_date:                Date.parse("28-02-2015")
+    )
+
+    assert_equal url, "dataValueSets?dataElementGroup=oDkJh5Ddh7d&startDate=2015-01-31&endDate=2015-02-28&orgUnitGroup=oRVt7g429ZO"
+  end
+
+  def test_should_build_list_url_for_ds_periods_ou
+    url = Dhis2.client.data_value_sets.build_list_url(
+      data_sets:         ["oRVt7g429ZO"],
+      periods:           ["oDkJh5Ddh7d"],
+      organisation_unit: "123456"
+    )
+
+    assert_equal url, "dataValueSets?dataSet=oRVt7g429ZO&period=oDkJh5Ddh7d&orgUnit=123456&children=true"
+  end
+
+  def test_should_build_list_url_for_ds_periods_ou
+    url = Dhis2.client.data_value_sets.build_list_url(
+      data_sets:         ["oRVt7g429ZO"],
+      periods:           ["oDkJh5Ddh7d"],
+      organisation_unit: "123456"
+    )
+
+    assert_equal url, "dataValueSets?dataSet=oRVt7g429ZO&period=oDkJh5Ddh7d&orgUnit=123456&children=true"
+  end
+
+  def test_should_build_list_url_for_ds_periods_multiple_ou
+    url = Dhis2.client.data_value_sets.build_list_url(
+      data_sets:         ["oRVt7g429ZO"],
+      periods:           ["oDkJh5Ddh7d"],
+      organisation_unit: ["123456","789456"]
+    )
+
+    assert_equal url, "dataValueSets?dataSet=oRVt7g429ZO&period=oDkJh5Ddh7d&orgUnit=123456&orgUnit=789456&children=true"
+  end
+
+  def test_list_values_orgunitsgroup_startdate_enddate_dataelement_group
+    # https://play.dhis2.org/demo/api/dataValueSets?dataElementGroup=oDkJh5Ddh7d&startDate=2015-01-31&endDate=2015-02-28&orgUnitGroup=oRVt7g429ZO
+
+    value_sets = Dhis2.client.data_value_sets.list(
+      organisation_unit_group: "oRVt7g429ZO",
+      data_element_groups:     ["oDkJh5Ddh7d"],
+      start_date:              Date.parse("31-1-2015"),
+      end_date:                Date.parse("28-02-2015")
+    )
+    assert_equal 1816, value_sets.values.size
   end
 
   def test_create_values
