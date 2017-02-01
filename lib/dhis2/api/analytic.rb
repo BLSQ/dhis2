44d776a7d3e841f2361a882537f2c5ddb4cc63a6
Alexandre Ravet <ravet.alexandre@gmail.com>
2017-02-01 15:34:22 +0100

    add filter to analytics

--- lib/dhis2/api/analytic.rb
+++ lib/dhis2/api/analytic.rb
@@ -2,13 +2,15 @@ module Dhis2
   module Api
     class Analytic < Base
       class << self
-        def list(client, periods:, organisation_units:, data_elements:)
-          params = RestClient::ParamsArray.new([
+        def list(client, periods:, organisation_units:, data_elements:, filter: nil)
+          params = [
             [:dimension, "ou:#{organisation_units}"],
             [:dimension, "dx:#{data_elements}"],
             [:dimension, "pe:#{periods}"]
-          ])
-          client.get(self.resource_name, params)
+          ]
+          params << [:filter, "#{filter}"] if filter
+
+          client.get(self.resource_name, RestClient::ParamsArray.new(params))
         end
       end
     end
