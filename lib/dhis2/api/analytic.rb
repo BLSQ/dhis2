74ce193ebe66eca0c6ddb73a28ee32f177fa869f
Alexandre Ravet <ravet.alexandre@gmail.com>
2017-02-15 14:22:36 +0100

    add dimension to analytics

--- lib/dhis2/api/analytic.rb
+++ lib/dhis2/api/analytic.rb
@@ -2,14 +2,14 @@ module Dhis2
   module Api
     class Analytic < Base
       class << self
-        def list(client, periods:, organisation_units:, data_elements:, filter: nil)
+        def list(client, periods:, organisation_units:, data_elements:, filter: nil, dimension: nil)
           params = [
             [:dimension, "ou:#{organisation_units}"],
             [:dimension, "dx:#{data_elements}"],
             [:dimension, "pe:#{periods}"]
           ]
           params << [:filter, "#{filter}"] if filter
-
+          params << [:dimension, "#{dimension}"] if dimension
           client.get(self.resource_name, RestClient::ParamsArray.new(params))
         end
       end
