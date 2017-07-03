991a88b92615a7f46fed4cdeb0c23c954a49194a
Stéphan Mestach <smestach@bluesquare.org>
2017-07-03 16:24:58 +0200

    Add support for querying events and added spec

--- lib/dhis2/api/event.rb
+++ lib/dhis2/api/event.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
+
 module Dhis2
   module Api
     class Event < Base
       def id
-        self.event
+        event
       end
       class << self
+        def format_query_parameters(options)
+
+          params = []
+          params.push([:page, options[:page]])                    if options[:page]
+          params.push([:pageSize, options[:page_size]])           if options[:page_size]
+          params.push([:fields, format_fields(options[:fields])]) if options[:fields]
+          params.concat(format_filter(options[:filter]))          if options[:filter]
+          params.push([:program, options[:program]])               if options[:program]
+          params.push([:orgUnit, options[:org_unit]])             if options[:org_unit]
+          params.push([:trackedEntityInstance, options[:tracked_entity_instance]]) if options[:tracked_entity_instance]
+
+          RestClient::ParamsArray.new(params)
+        end
+
         def create(client, tuples)
           begin
             body = { resource_name.to_sym => tuples }
