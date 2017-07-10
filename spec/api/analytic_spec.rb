06fed22d156e69466b458c62783710c9ad58f532
mestachs <mestachs>
2017-07-10 20:21:51 +0200

    create data values

---
+++ spec/api/analytic_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe Dhis2::Api::Analytic do
+  it "should list by dimension ou, dx, pe and extra filter" do
+    stub_request(:get, "https://play.dhis2.org/demo/api/analytics?dimension=pe:%5B%222016Q1%22,%20%222016%22%5D").
+         to_return(status: 200, body: "", headers: {})
+
+
+    Dhis2.client.analytics.list(
+      periods:            %w[2016Q1 2016],
+      organisation_units: %w[123 456],
+      data_elements:      ["de-987"]
+    )
+  end
+end
--- spec/api/data_values_set_spec.rb
+++ spec/api/data_values_set_spec.rb
@@ -4,6 +4,18 @@
 require "spec_helper"
 
 RSpec.describe Dhis2::Api::DataValueSet do
+
+  describe "#create" do
+    it "should return import status" do
+      stub_request(:post, "https://play.dhis2.org/demo/api/dataValueSets").
+             with(body: "{\"dataValues\":{\"de\":\"de\"}}").
+             to_return(status: 200, body: "")
+
+      status = Dhis2.client.data_value_sets.create( {de: "de"})
+      expect(status).to be_a(Dhis2::Status)
+    end
+  end
+
   describe "#list" do
     it "should support by data_sets, orgUnit, periods" do
       stub_request(:get, "https://play.dhis2.org/demo/api/dataValueSets?children=true&dataSet=ds1&orgUnit=ou1&period=2017Q1")
@@ -33,14 +45,16 @@ RSpec.describe Dhis2::Api::DataValueSet do
                           "&endDate=2015-02-28",
                           "&orgUnitGroup=ougroup1",
                           "&startDate=2015-01-31"].join)
-        .to_return(status: 200, body: "", headers: {})
+        .to_return(status: 200, body: fixture_content(:dhis2, "data_value_sets.json"))
 
-      Dhis2.client.data_value_sets.list(
+      data_values = Dhis2.client.data_value_sets.list(
         organisation_unit_group: "ougroup1",
         data_element_groups:     ["de_group1"],
         start_date:              Date.parse("31-1-2015"),
         end_date:                Date.parse("28-02-2015")
       )
+
+      expect(data_values.values.first).to be_a OpenStruct
     end
   end
 end
