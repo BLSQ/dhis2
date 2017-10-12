6200cb711e219e17abe9abc6c228336ec8cd3c1e
Benjamin Roth <benjamin@rubyist.fr>
2017-10-12 15:55:39 +0200

    more specs

--- spec/lib/dhis2/api/data_element_spec.rb
+++ spec/lib/dhis2/api/data_element_spec.rb
@@ -11,47 +11,45 @@ describe Dhis2::Api::DataElement do
   describe "#create" do
     let(:data_element_1) do
       {
-        "name"                => "TesTesT1148",
-        "shortName"           => "TTT1148",
-        "code"                => "TTT1148",
-        "domainType"          => "AGGREGATE",
-        "valueType"           => "NUMBER",
-        "aggregationType"     => "SUM",
-        "type"                => "int",
-        "aggregationOperator" => "SUM",
-        "zeroIsSignificant"   => true,
-        "categoryCombo"       => {
-          "id" => "p0KPaWEg3cf", "name" => "default"
+        name:                 "TesTesT1148",
+        short_name:           "TTT1148",
+        code:                 "TTT1148",
+        domain_type:          "AGGREGATE",
+        value_type:           "NUMBER",
+        aggregation_type:     "SUM",
+        type:                 "int",
+        aggregation_operator: "SUM",
+        zero_is_significant:   true,
+        category_combo: {
+          id: "p0KPaWEg3cf", name: "default"
         }
       }
     end
 
     let(:data_element_2) do
       {
-        "name" => "TesTesT7712",
-         "shortName" => "TTT7712",
-         "code" => "TTT7712", "domainType" => "AGGREGATE",
-         "valueType" => "NUMBER",
-         "aggregationType" => "SUM",
-         "type" => "int",
-         "aggregationOperator" => "SUM",
-         "zeroIsSignificant"   => true,
-         "categoryCombo" => {
-           "id" => "p0KPaWEg3cf", "name" => "default"
-         }
+        name:                 "TesTesT7712",
+        short_name:           "TTT7712",
+        code:                 "TTT7712",
+        domain_type:          "AGGREGATE",
+        value_type:           "NUMBER",
+        aggregation_type:     "SUM",
+        type:                 "int",
+        aggregation_operator: "SUM",
+        zero_is_significant:  true,
+        category_combo: {
+          id: "p0KPaWEg3cf", name: "default"
+        }
       }
     end
+
+    let(:elements) { [ data_element_1, data_element_2 ] }
 
     it "should create data element with default category combo if not present and preheatCache=false to speed on large dhis2 instances" do
       combo_stub = stub_request(:get, "https://play.dhis2.org/demo/api/categoryCombos?fields=:all&filter=name:eq:default")
                    .to_return(status: 200, body: fixture_content(:dhis2, "category_combos_default.json"))
 
       stub_data_elements_create_on_meta
-
-      elements = [
-        { name: data_element_1["name"], short_name: data_element_1["shortName"] },
-        { name: data_element_2["name"], short_name: data_element_2["shortName"] }
-      ]
 
       status = Dhis2.client.data_elements.create(elements)
 
@@ -62,14 +60,7 @@ describe Dhis2::Api::DataElement do
 
     def stub_data_elements_create_on_meta
       stub_request(:post, "https://play.dhis2.org/demo/api/metadata?preheatCache=false")
-        .with(
-          body: JSON.generate(
-            "dataElements" => [
-              data_element_1,
-              data_element_2
-            ]
-          )
-        )
+        .with(body: JSON.generate("dataElements" => Dhis2::Client.deep_change_case(elements, :camelize)))
         .to_return(status: 200, body: fixture_content(:dhis2, "data_element_create_status.json"))
     end
   end
--- spec/lib/dhis2/api/event_spec.rb
+++ spec/lib/dhis2/api/event_spec.rb
@@ -6,4 +6,12 @@ describe Dhis2::Api::Event do
   it_behaves_like "a DHIS2 listable resource"
   it_behaves_like "a DHIS2 findable resource"
   it_behaves_like "a DHIS2 deletable resource"
+
+  describe "#id" do
+    let(:event) { described_class.new(Dhis2.client, { event: 'abc' }) }
+
+    it "returns event as id" do
+      expect(event.id).to eq event.event
+    end
+  end
 end
