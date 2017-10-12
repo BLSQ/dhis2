289aa10bbb6dce9a631585619ce0d4301559bd57
Stéphan Mestach <smestach@bluesquare.org>
2017-10-12 13:01:32 +0200

    Allow to create an orgunit groupset

--- lib/dhis2/api/organisation_unit_group_set.rb
+++ lib/dhis2/api/organisation_unit_group_set.rb
@@ -1,6 +1,14 @@
 module Dhis2
   module Api
     class OrganisationUnitGroupSet < Base
+      class << self
+        def create(client, orgunit_group_sets)
+          orgunit_group_sets = [orgunit_group_sets].flatten
+          response = client.post("metadata",
+                                 organisation_unit_group_sets: orgunit_group_sets)
+          Dhis2::Status.new(response)
+        end
+      end
     end
   end
 end
