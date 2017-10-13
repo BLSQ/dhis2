c2520dbca3f5244a37a9ee10ea3649c2b843edd7
mestachs <mestachs>
2017-10-13 21:03:40 +0200

    Add support to create Organisation unit group set

--- lib/dhis2/api/organisation_unit_group_set.rb
+++ lib/dhis2/api/organisation_unit_group_set.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
+
 module Dhis2
   module Api
     class OrganisationUnitGroupSet < Base
       class << self
         def create(client, orgunit_group_sets)
-          orgunit_group_sets = [orgunit_group_sets].flatten
-          response = client.post("metadata",
-                                 organisation_unit_group_sets: orgunit_group_sets)
+          response = client.post(
+            "metadata",
+            organisation_unit_group_sets: ensure_array(orgunit_group_sets)
+          )
           Dhis2::Status.new(response)
         end
       end
