54bc68dd124a64fa5b8fdf733bc50ad4b04ecdb0
Benjamin Roth <benjamin@rubyist.fr>
2017-10-13 14:08:14 +0200

    add spec to check defaults are enforced

--- spec/lib/dhis2/api/data_element_spec.rb
+++ spec/lib/dhis2/api/data_element_spec.rb
@@ -30,6 +30,17 @@ describe Dhis2::Api::DataElement do
       {
         name:                 "TesTesT7712",
         short_name:           "TTT7712",
+        aggregation_operator: "SUM",
+        category_combo:       {
+          id: "p0KPaWEg3cf", name: "default"
+        }
+      }
+    end
+
+    let(:data_element_2_with_defaults) do
+      {
+        name:                 "TesTesT7712",
+        short_name:           "TTT7712",
         code:                 "TTT7712",
         domain_type:          "AGGREGATE",
         value_type:           "NUMBER",
@@ -43,15 +54,13 @@ describe Dhis2::Api::DataElement do
       }
     end
 
-    let(:elements) { [data_element_1, data_element_2] }
-
     it "should create data element with default category combo if not present and preheatCache=false to speed on large dhis2 instances" do
       combo_stub = stub_request(:get, "https://play.dhis2.org/demo/api/categoryCombos?fields=:all&filter=name:eq:default")
                    .to_return(status: 200, body: fixture_content(:dhis2, "category_combos_default.json"))
 
       stub_data_elements_create_on_meta
 
-      status = Dhis2.client.data_elements.create(elements)
+      status = Dhis2.client.data_elements.create([data_element_1, data_element_2])
 
       expect(status.success?).to eq true
       expect(status.total_imported).to eq 2
@@ -60,7 +69,7 @@ describe Dhis2::Api::DataElement do
 
     def stub_data_elements_create_on_meta
       stub_request(:post, "https://play.dhis2.org/demo/api/metadata?preheatCache=false")
-        .with(body: JSON.generate("dataElements" => Dhis2::Case.deep_change(elements, :camelize)))
+        .with(body: JSON.generate("dataElements" => Dhis2::Case.deep_change([data_element_1, data_element_2_with_defaults], :camelize)))
         .to_return(status: 200, body: fixture_content(:dhis2, "data_element_create_status.json"))
     end
   end
