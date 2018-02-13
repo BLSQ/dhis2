18b729b420f5017ecc2a1da26168959cbbe3be60
Benjamin Roth <benjamin@rubyist.fr>
2018-02-13 14:42:23 +0100

    override data value set list method

--- lib/dhis2/api/shared/data_value_set.rb
+++ lib/dhis2/api/shared/data_value_set.rb
@@ -9,6 +9,17 @@ module Dhis2
         end
 
         module ClassMethods
+          def list(client, options = {}, raw = false)
+            response = super(client, options, raw)
+            if raw
+              response["dataValues"]
+            else
+              response["data_values"].map do |elt|
+                OpenStruct.new(elt)
+              end
+            end
+          end
+
           private
 
           def instance_creation_success?(response)
