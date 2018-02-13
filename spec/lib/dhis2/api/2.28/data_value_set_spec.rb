3d8094fe612741ba9abcfbb6c77a54f9335d181e
Benjamin Roth <benjamin@rubyist.fr>
2018-02-13 15:29:47 +0100

    shared specs for data value set

--- spec/lib/dhis2/api/2.28/data_value_set_spec.rb
+++ spec/lib/dhis2/api/2.28/data_value_set_spec.rb
@@ -6,22 +6,5 @@ describe Dhis2::Api::Version228::DataValueSet do
   let(:client) { build_client("2.28") }
 
   it_behaves_like "a DHIS2 creatable resource", version: "2.28", nil_id: true
-
-  describe ".list" do
-    let(:json_response) { fixture_content("2.28", "list", "data_value_sets.json") }
-
-    before do
-      stub_request(:get, "https://play.dhis2.org/demo/api/dataValueSets?orgUnit=eYcb2MlHGhk&period=201601&dataElementGroup=UJabOSRvwHI")
-        .to_return(status: 200, body: json_response)
-    end
-
-    it "returns expected json" do
-      result = client.data_value_sets.list(
-        org_unit:           "eYcb2MlHGhk",
-        period:             "201601",
-        data_element_group: "UJabOSRvwHI"
-      )
-      expect(result).to be_an Array
-    end
-  end
+  it_behaves_like "a DHIS2 data value list", version: "2.28"
 end
