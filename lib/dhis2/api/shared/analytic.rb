c937b114c9b43e8b2c5bcb2fe49f04fb9403db3e
Boris Rorsvort <boris@rorsvort.com>
2018-07-13 13:04:50 +0200

    Add raw option to analytics (#49)

--- lib/dhis2/api/shared/analytic.rb
+++ lib/dhis2/api/shared/analytic.rb
@@ -4,7 +4,7 @@ module Dhis2
   module Api
     module Shared
       class Analytic
-        def self.list(client, periods:, organisation_units:, data_elements: nil, filter: nil)
+        def self.list(client, periods:, organisation_units:, data_elements: nil, filter: nil, raw: false)
           params = [
             [:dimension, "ou:#{organisation_units}"],
             [:dimension, "pe:#{periods}"]
@@ -12,7 +12,7 @@ module Dhis2
           params << [:dimension, "dx:#{data_elements}"] if data_elements
           params << [:filter, filter.to_s] if filter
 
-          client.get(path: "analytics", query_params: RestClient::ParamsArray.new(params))
+          client.get(path: "analytics", query_params: RestClient::ParamsArray.new(params), raw: raw)
         end
       end
     end
--- spec/support/analytic_shared_example.rb
+++ spec/support/analytic_shared_example.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 RSpec.shared_examples "an analytic endpoint" do |version:|
   # actually webmock doesnt work with same param multiple times
   # see https://github.com/bblimke/webmock/issues/330
@@ -10,8 +11,8 @@ RSpec.shared_examples "an analytic endpoint" do |version:|
       .to_return(status: 200, body: "", headers: {})
 
     client.analytics.list(
-      periods:            %w(2016Q1 2016),
-      organisation_units: %w(123 456),
+      periods:            %w[2016Q1 2016],
+      organisation_units: %w[123 456],
       data_elements:      ["de-987"]
     )
   end
@@ -21,10 +22,11 @@ RSpec.shared_examples "an analytic endpoint" do |version:|
     let(:organisation_unit_id) { "vWbkYPRmKyS" }
     let(:period) { "2016" }
 
-    it "returns expected data" do
-      stub_request(:get, "https://play.dhis2.org/demo/api/analytics?dimension=ou%3AvWbkYPRmKyS&dimension=pe%3A2016&dimension=dx%3Ah0xKKjijTdI")
-        .to_return(status: 200, body: shared_content("analytics.json"))
+    before do
+      stub_api_request
+    end
 
+    it "returns expected data" do
       row = client.analytics.list(
         data_elements:      data_element_id,
         organisation_units: organisation_unit_id,
@@ -34,6 +36,24 @@ RSpec.shared_examples "an analytic endpoint" do |version:|
       expect(data_element_id).to      eq row[0]
       expect(organisation_unit_id).to eq row[1]
       expect(period).to               eq row[2]
+    end
+
+    context "with raw data option" do
+      it "returns expected data " do
+        row = client.analytics.list(
+          data_elements:      data_element_id,
+          organisation_units: organisation_unit_id,
+          periods:            period,
+          raw:                true
+        )
+        expect(row["metaData"]["items"][data_element_id]["name"]).to eq("Expected pregnancies")
+        expect(row["rows"].first).to eq([data_element_id, organisation_unit_id, period, "3583.0"])
+      end
+    end
+
+    def stub_api_request
+      stub_request(:get, "https://play.dhis2.org/demo/api/analytics?dimension=ou%3AvWbkYPRmKyS&dimension=pe%3A2016&dimension=dx%3Ah0xKKjijTdI")
+        .to_return(status: 200, body: shared_content("analytics.json"))
     end
   end
 end
