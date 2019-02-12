91bd6f817f995f73f195ab1754388257609d925b
Boris Rorsvort <boris@rorsvort.com>
2019-02-12 12:07:43 +0100

    Add support for program indicators (#57)

--- lib/dhis2/client.rb
+++ lib/dhis2/client.rb
@@ -116,6 +116,10 @@ module Dhis2
       @programs ||= CollectionWrapper.new("Program", self)
     end
 
+    def program_indicators
+      @program_indicators ||= CollectionWrapper.new("ProgramIndicator", self)
+    end
+
     def program_indicator_groups
       @program_indicator_groups ||= CollectionWrapper.new("ProgramIndicatorGroup", self)
     end
