61235c7afcc7b34ccfef0bd62d8b58aaee2ca1b6
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-04-20 15:02:29 +0200

    avoid forced period and ous dimensions

--- lib/dhis2/api/shared/analytic.rb
+++ lib/dhis2/api/shared/analytic.rb
@@ -4,11 +4,10 @@ module Dhis2
   module Api
     module Shared
       class Analytic
-        def self.list(client, periods:, organisation_units:, data_elements: nil, filter: nil, raw: false)
-          params = [
-            [:dimension, "ou:#{organisation_units}"],
-            [:dimension, "pe:#{periods}"]
-          ]
+        def self.list(client, periods: nil, organisation_units: nil, data_elements: nil, filter: nil, raw: false)
+          params = []
+          params << [:dimension, "pe:#{periods}"] if periods
+          params << [:dimension, "ou:#{organisation_units}"] if organisation_units
           params << [:dimension, "dx:#{data_elements}"] if data_elements
           params << [:filter, filter.to_s] if filter
 
